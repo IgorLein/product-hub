8306ede75f36faeda1065b74a05eefd04cca8f37
Igor Lein <igor.lein89@gmail.com>
2026-05-15 23:30:20 +0200

    implement getting tags by category key

--- catalog-service/src/controllers/category.controller.ts
+++ catalog-service/src/controllers/category.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import * as categoryService from '../services/category.service.js';
+import * as tagService from '../services/tag.service.js';
 
 export async function getCategories(
   _req: Request,
@@ -13,3 +14,22 @@ export async function getCategories(
     next(error);
   }
 }
+
+export async function getCategoryTags(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const categoryKey = req.params.categoryKey;
+    if (!categoryKey || Array.isArray(categoryKey)) {
+      res.status(400).json({ message: 'Invalid category key' });
+      return;
+    }
+
+    const tags = await tagService.getTagsByCategoryKey(categoryKey);
+    res.status(200).json(tags);
+  } catch (error) {
+    next(error);
+  }
+};
--- catalog-service/src/services/tag.service.ts
+++ catalog-service/src/services/tag.service.ts
@@ -1,6 +1,6 @@
 import { Tag } from '../models/tag.model.js';
 import { TagDto, mapTagToDto } from '../mappers/tag.mapper.js';
-import { Product } from '../models/index.js';
+import { Category, Product } from '../models/index.js';
 
 export async function getTags(): Promise<TagDto[]> {
   const tags = await Tag.findAll({
@@ -25,3 +25,27 @@ export async function getTagsByProductId(productId: number): Promise<TagDto[]> {
 
   return tags.map(mapTagToDto);
 };
+
+export async function getTagsByCategoryKey(categoryKey: string): Promise<TagDto[]> {
+  const tags = await Tag.findAll({
+    include: [
+      {
+        model: Product,
+        as: 'products',
+        required: true,
+        include: [
+          {
+            model: Category,
+            as: 'category',
+            required: true,
+            where: { key: categoryKey },
+          },
+        ],
+        through: { attributes: [] },
+      },
+    ],
+    order: [["id", "ASC"]],
+  });
+
+  return tags.map(mapTagToDto);
+}
